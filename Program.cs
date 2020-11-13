b0f42a6652ef126db4bf3785b95257f84d39b37f
Anisha-Badhan <68878676+Anisha-Badhan@users.noreply.github.com>
2020-11-13 18:16:14 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            hello
+            hello how are you
             Console.WriteLine(Check4CHash());
         }
 
