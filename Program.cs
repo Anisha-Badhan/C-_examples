10fa8e3ab259d91ecf51683e9db2bb0e28aef0c5
Anisha-Badhan <68878676+Anisha-Badhan@users.noreply.github.com>
2020-11-17 18:18:24 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp1
 {
@@ -6,16 +6,24 @@ namespace ConsoleApp1
     {
         // Please don't use console.writeline inside the methods, call the methods from Main() and pass parameters.
 
+
         static void Main(string[] args)
         {
-            MyMethod(4, 5);
+            Console.WriteLine(print_subStr("HelloWorld", "1 4"));
+            String[] arr = { "Julia", "Samantha", "Samantha_21", "1samantha", "Samatha?10_2A" };
+            name_validity(5, arr);
         }
 
+        /*
+        * @param int a
+        * @param int b
+        * @return int
+       */
         public static int MyMethod(int a, int b)
         {
 
             // This is good, but can you try to use Ternary Opertor here to make it compact.
-            
+
             return (a == b ? 3 * (a + b) : a + b);
         }
 
@@ -25,12 +33,21 @@ namespace ConsoleApp1
             return (a == 30 || b == 30 || (a + b) == 30);
         }
 
+        /*
+        * @param String str
+        * @return String
+       */
         public static String MyString(String str)
         {
             // Wrap the condition in () --> (str.StartsWith("IF") || str.StartsWith("if") || str.StartsWith("If"))
             return (str.StartsWith("IF") || str.StartsWith("if") || str.StartsWith("If") ? str : String.Concat("If", str));
         }
 
+        /*
+        * @param String str
+        * @param int index
+        * @return String
+       */
         public static String RmoveChr(String str, int index)
         {
             // Wrap the condition in ()
@@ -38,22 +55,38 @@ namespace ConsoleApp1
         }
 
         // Look for a Method called Substring() and try to do this question with that.
+        /*
+        * @param String str
+        * @return String
+       */
         public static String Exchange(String str)
         {
             return str.Length > 1 ? str.Substring(str.Length - 1) + str.Substring(1, str.Length - 2) + str.Substring(0, 1) : str;
         }
 
+        /*
+        * @param String str
+        * @return String
+       */
         public static String Repeat4times(String str)
         {
             return str.Length < 2 ? str : str.Substring(0, 2) + str.Substring(0, 2) + str.Substring(0, 2) + str.Substring(0, 2);
         }
 
+        /*
+        * @param String str
+        * @return String
+       */
         public static String Addchar(String str)
         {
             String s = str.Substring(str.Length - 1);
             return s + str + s;
         }
 
+        /*
+        * @param str
+        * @return String
+       */
         public static String AddStr(String str)
         {
             int StrLen = str.Length;
@@ -62,24 +95,39 @@ namespace ConsoleApp1
             return (StrLen < 3 ? str + str + str : str.Substring(0, 3) + str + str.Substring(0, 3));
         }
 
+        /*
+        * @param String str
+        * @return boolean
+       */
         public static bool Check4CHash(String str)
         {
             return (str.Equals("C#")) || (str.StartsWith("C#") && str[2] == ' ');
         }
 
+        /*
+        * @param int x
+        * @param int y
+        * @param int z
+        * @return boolean
+       */
         public static bool TestRange(int x, int y, int z)
         {
             return (x >= 20 && x <= 50) || (y >= 20 && y <= 50) || (z >= 20 && z <= 50);
         }
-        
+
+        /*
+        * @param array arr
+        * @param int d
+        * @return void
+       */
         public static void ArrRotation(int[] arr, int d)
         {
-            int[] arr1 = new int[arr.Length] ;
-            for(int i = d; i < arr.Length; i++)
+            int[] arr1 = new int[arr.Length];
+            for (int i = d; i < arr.Length; i++)
             {
-                arr1[i-d] = arr[i];
+                arr1[i - d] = arr[i];
             }
-            for (int i = 0; i < d; i++) 
+            for (int i = 0; i < d; i++)
             {
                 arr1.SetValue(arr[i], arr.Length - d + i);
             }
@@ -88,8 +136,34 @@ namespace ConsoleApp1
                 System.Console.Write("{0} ", i);
             }
         }
+
+        /*
+         * @param String
+         * @param String
+         * @return String
+         */
+        public static String print_subStr(String str, String index)
+        {
+            String[] indices = (index.Split(" "));
+            int start = Convert.ToInt32(indices[0]);
+            int end = Convert.ToInt32(indices[1]);
+            int n = str.Length;
+            foreach (Char c in str)
+            {
+                if (!Char.IsLetter(c))
+                {
+                    Console.WriteLine("please enter a valid string");
+                }
+            }
+            if (start < 0 || start >= end || end > n)
+            {
+                Console.WriteLine("Enter valid indices");
+            }
+            int length = end - start + 1;
+            return str.Substring(start, length);
+        }
+
     }
-            
-} 
+}
         
 
